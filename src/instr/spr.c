f878f89c7110fdafd12c0346ce82438fd307ec8a
techflashYT <officialTechflashYT@gmail.com>
2026-02-12 09:36:01 -0700

    spr: support BATs

--- src/instr/spr.c
+++ src/instr/spr.c
@@ -50,6 +50,50 @@ static void do_mtspr(struct _ppcemu_state *state, uint rS, uint sprn, uint Rc) {
 
 		break;
 	}
+	case PPCEMU_SPRN_IBAT0U:
+	case PPCEMU_SPRN_IBAT0L:
+	case PPCEMU_SPRN_IBAT1U:
+	case PPCEMU_SPRN_IBAT1L:
+	case PPCEMU_SPRN_IBAT2U:
+	case PPCEMU_SPRN_IBAT2L:
+	case PPCEMU_SPRN_IBAT3U:
+	case PPCEMU_SPRN_IBAT3L:
+	case PPCEMU_SPRN_DBAT0U:
+	case PPCEMU_SPRN_DBAT0L:
+	case PPCEMU_SPRN_DBAT1U:
+	case PPCEMU_SPRN_DBAT1L:
+	case PPCEMU_SPRN_DBAT2U:
+	case PPCEMU_SPRN_DBAT2L:
+	case PPCEMU_SPRN_DBAT3U:
+	case PPCEMU_SPRN_DBAT3L: {
+		state->sprs[ppcemu_sprn_to_idx(sprn)] = state->gpr[rS];
+		break;
+	}
+	case PPCEMU_SPRN_IBAT4U:
+	case PPCEMU_SPRN_IBAT4L:
+	case PPCEMU_SPRN_IBAT5U:
+	case PPCEMU_SPRN_IBAT5L:
+	case PPCEMU_SPRN_IBAT6U:
+	case PPCEMU_SPRN_IBAT6L:
+	case PPCEMU_SPRN_IBAT7U:
+	case PPCEMU_SPRN_IBAT7L:
+	case PPCEMU_SPRN_DBAT4U:
+	case PPCEMU_SPRN_DBAT4L:
+	case PPCEMU_SPRN_DBAT5U:
+	case PPCEMU_SPRN_DBAT5L:
+	case PPCEMU_SPRN_DBAT6U:
+	case PPCEMU_SPRN_DBAT6L:
+	case PPCEMU_SPRN_DBAT7U:
+	case PPCEMU_SPRN_DBAT7L: {
+		if (!(state->caps & CAPS_UPPER_BATS)) {
+			puts("Attempted to write to upper BATs on an unsupported CPU");
+			exception_fire(state, EXCEPTION_PROGRAM);
+		}
+		else
+			state->sprs[ppcemu_sprn_to_idx(sprn)] = state->gpr[rS];
+
+		break;
+	}
 	default: {
 		printf("Unknown SPR write %d\r\n", sprn);
 		exception_fire(state, EXCEPTION_PROGRAM);
@@ -88,6 +132,50 @@ static void do_mfspr(struct _ppcemu_state *state, uint rS, uint sprn, uint Rc) {
 	case PPCEMU_SPRN_HID4: {
 		if (!(state->caps & CAPS_HID4)) {
 			puts("Attempted to read HID4 on unsupported CPU");
+			exception_fire(state, EXCEPTION_PROGRAM);
+		}
+		else
+			state->gpr[rS] = state->sprs[ppcemu_sprn_to_idx(sprn)];
+
+		break;
+	}
+	case PPCEMU_SPRN_IBAT0U:
+	case PPCEMU_SPRN_IBAT0L:
+	case PPCEMU_SPRN_IBAT1U:
+	case PPCEMU_SPRN_IBAT1L:
+	case PPCEMU_SPRN_IBAT2U:
+	case PPCEMU_SPRN_IBAT2L:
+	case PPCEMU_SPRN_IBAT3U:
+	case PPCEMU_SPRN_IBAT3L:
+	case PPCEMU_SPRN_DBAT0U:
+	case PPCEMU_SPRN_DBAT0L:
+	case PPCEMU_SPRN_DBAT1U:
+	case PPCEMU_SPRN_DBAT1L:
+	case PPCEMU_SPRN_DBAT2U:
+	case PPCEMU_SPRN_DBAT2L:
+	case PPCEMU_SPRN_DBAT3U:
+	case PPCEMU_SPRN_DBAT3L: {
+		state->gpr[rS] = state->sprs[ppcemu_sprn_to_idx(sprn)];
+		break;
+	}
+	case PPCEMU_SPRN_IBAT4U:
+	case PPCEMU_SPRN_IBAT4L:
+	case PPCEMU_SPRN_IBAT5U:
+	case PPCEMU_SPRN_IBAT5L:
+	case PPCEMU_SPRN_IBAT6U:
+	case PPCEMU_SPRN_IBAT6L:
+	case PPCEMU_SPRN_IBAT7U:
+	case PPCEMU_SPRN_IBAT7L:
+	case PPCEMU_SPRN_DBAT4U:
+	case PPCEMU_SPRN_DBAT4L:
+	case PPCEMU_SPRN_DBAT5U:
+	case PPCEMU_SPRN_DBAT5L:
+	case PPCEMU_SPRN_DBAT6U:
+	case PPCEMU_SPRN_DBAT6L:
+	case PPCEMU_SPRN_DBAT7U:
+	case PPCEMU_SPRN_DBAT7L: {
+		if (!(state->caps & CAPS_UPPER_BATS)) {
+			puts("Attempted to read the upper BATs on an unsupported CPU");
 			exception_fire(state, EXCEPTION_PROGRAM);
 		}
 		else
--- src/spr.h
+++ src/spr.h
@@ -10,7 +10,7 @@
 #include <assert.h>
 #include <ppcemu/spr.h>
 
-#define MAX_SPR_COUNT 16
+#define MAX_SPR_COUNT 48
 static inline int ppcemu_sprn_to_idx(int sprn) {
 	switch (sprn) {
 	case PPCEMU_SPRN_LR: return 0;
@@ -29,6 +29,38 @@ static inline int ppcemu_sprn_to_idx(int sprn) {
 	case PPCEMU_SPRN_HID0: return 13;
 	case PPCEMU_SPRN_HID2_GEKKO: return 14;
 	case PPCEMU_SPRN_HID4: return 15;
+	case PPCEMU_SPRN_IBAT0U: return 16;
+	case PPCEMU_SPRN_IBAT0L: return 17;
+	case PPCEMU_SPRN_IBAT1U: return 18;
+	case PPCEMU_SPRN_IBAT1L: return 19;
+	case PPCEMU_SPRN_IBAT2U: return 20;
+	case PPCEMU_SPRN_IBAT2L: return 21;
+	case PPCEMU_SPRN_IBAT3U: return 22;
+	case PPCEMU_SPRN_IBAT3L: return 23;
+	case PPCEMU_SPRN_DBAT0U: return 24;
+	case PPCEMU_SPRN_DBAT0L: return 25;
+	case PPCEMU_SPRN_DBAT1U: return 26;
+	case PPCEMU_SPRN_DBAT1L: return 27;
+	case PPCEMU_SPRN_DBAT2U: return 28;
+	case PPCEMU_SPRN_DBAT2L: return 29;
+	case PPCEMU_SPRN_DBAT3U: return 30;
+	case PPCEMU_SPRN_DBAT3L: return 31;
+	case PPCEMU_SPRN_IBAT4U: return 32;
+	case PPCEMU_SPRN_IBAT4L: return 33;
+	case PPCEMU_SPRN_IBAT5U: return 34;
+	case PPCEMU_SPRN_IBAT5L: return 35;
+	case PPCEMU_SPRN_IBAT6U: return 36;
+	case PPCEMU_SPRN_IBAT6L: return 37;
+	case PPCEMU_SPRN_IBAT7U: return 38;
+	case PPCEMU_SPRN_IBAT7L: return 39;
+	case PPCEMU_SPRN_DBAT4U: return 40;
+	case PPCEMU_SPRN_DBAT4L: return 41;
+	case PPCEMU_SPRN_DBAT5U: return 42;
+	case PPCEMU_SPRN_DBAT5L: return 43;
+	case PPCEMU_SPRN_DBAT6U: return 44;
+	case PPCEMU_SPRN_DBAT6L: return 45;
+	case PPCEMU_SPRN_DBAT7U: return 46;
+	case PPCEMU_SPRN_DBAT7L: return 47;
 	default: assert(!"Unreachable");
 	}
 }
