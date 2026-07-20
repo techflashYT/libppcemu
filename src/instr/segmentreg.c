0d02d57d70b1c60cb4f0b3fbdc1d174c1d1e0876
techflashYT <officialTechflashYT@gmail.com>
2026-07-20 12:49:45 -0500

    segmentreg: use real logging

--- src/instr/segmentreg.c
+++ src/instr/segmentreg.c
@@ -5,14 +5,16 @@
  */
 
 #include <stdio.h>
+#define LOG_LEVEL misc_loglevel
 #include "../state.h"
+#include "../log.h"
 
 void do_mtsr(struct _ppcemu_state *state, uint sreg, uint rS) {
 	u32 val;
 
 	val = state->gpr[rS];
 	if (!(val & 0x80000000))
-		puts("Writing a non-direct-store segment which will not be interpreted!!!");
+		warn("Writing a non-direct-store segment which will not be interpreted!!!");
 
 	state->sr[sreg] = val;
 }
