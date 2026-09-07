c9250e3c1ab629b8bf947d5da1126f26229836c4
techflashYT <officialTechflashYT@gmail.com>
2026-09-07 18:39:31 -0500

    decode,instr,ps: ps_rsqrte

--- src/instr.h
+++ src/instr.h
@@ -294,6 +294,7 @@ extern void do_ps_mul(struct _ppcemu_state *state, uint frD, uint frA, uint frC,
 extern void do_ps_madd(struct _ppcemu_state *state, uint frD, uint frA, uint frB, uint frC, uint Rc);
 extern void do_ps_msub(struct _ppcemu_state *state, uint frD, uint frA, uint frB, uint frC, uint Rc);
 extern void do_ps_neg(struct _ppcemu_state *state, uint frD, uint frB, uint Rc);
+extern void do_ps_rsqrte(struct _ppcemu_state *state, uint frD, uint frB, uint Rc);
 extern void do_ps_sub(struct _ppcemu_state *state, uint frD, uint frA, uint frB, uint Rc);
 extern void do_ps_cmpo0(struct _ppcemu_state *state, uint crfD, uint frA, uint frB);
 extern void do_ps_add(struct _ppcemu_state *state, uint frD, uint frA, uint frB, uint Rc);
